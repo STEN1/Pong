2cc84724d9aa60b3a43f8c33dbe851f6cf7feb5a
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2021-06-08 22:07:50 +0200

    p114

--- Pong/Game.h
+++ Pong/Game.h
@@ -6,12 +6,33 @@ struct Vec2
 {
 	T x;
 	T y;
+
+	Vec2& operator+=(const Vec2& rhs)
+	{
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+	Vec2 operator*(const float& rhs) const
+	{
+		return { x * rhs, y * rhs };
+	}
 };
 
-SDL_Window* mWindow{nullptr};
-SDL_Renderer* mRenderer{nullptr};
+SDL_Window* window{nullptr};
+SDL_Renderer* renderer{nullptr};
 
-bool mIsRunning{true};
+bool is_running{true};
 
-Vec2<float> ballPos{200.f, 200.f};
-Vec2<float> paddlePos{30.f, 300.f};
+Uint32 ticks_count{0};
+
+Vec2<float> ball_pos{200.f, 200.f};
+Vec2<float> ball_vel{-200.f, 235.f};
+Vec2<float> paddle_pos{30.f, 300.f};
+
+const int thickness{20};
+
+int8_t paddle_dir{0};
+const int paddle_h{100};
+
+float paddle_speed{300.f};
--- Pong/Main.cpp
+++ Pong/Main.cpp
@@ -10,7 +10,7 @@ void process_input()
 		switch (event.type)
 		{
 			case SDL_QUIT:
-				mIsRunning = false;
+				is_running = false;
 				break;
 			default:
 				break;
@@ -21,45 +21,92 @@ void process_input()
 
 	if (keyboardState[SDL_SCANCODE_ESCAPE])
 	{
-		mIsRunning = false;
+		is_running = false;
 	}
+
+	paddle_dir = 0;
+	
+	if (keyboardState[SDL_SCANCODE_W])
+	{
+		paddle_dir -= 1;
+	}
+
+	if (keyboardState[SDL_SCANCODE_S])
+	{
+		paddle_dir += 1;
+	}
+}
+
+float update_delta_time()
+{
+	// limit framerate to 16ms (60fps)
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticks_count + 16))
+	{
+	}
+	
+	// convert from ms to s
+	float deltaTime = (SDL_GetTicks() - ticks_count) / 1000.f;
+	// tick count for next frame
+	ticks_count = SDL_GetTicks();
+
+	// clamp deltaTime. dont want delaTime to be to large after debugging.
+	if (deltaTime > 0.05f)
+	{
+		deltaTime = 0.05f;
+	}
+
+	return deltaTime;
 }
 
 void update_game()
 {
+	const float deltaTime = update_delta_time();
+
+	paddle_pos.y += static_cast<float>(paddle_dir) * paddle_speed * deltaTime;
+
+	if (paddle_pos.y < paddle_h/2.f + thickness)
+	{
+		paddle_pos.y = paddle_h/2.f + thickness;
+	}
+	else if (paddle_pos.y > 768 - (paddle_h/2.f + thickness))
+	{
+		paddle_pos.y = 768 - (paddle_h/2.f + thickness);
+	}
+
+	ball_pos += ball_vel * deltaTime;
 }
 
 void generate_output()
 {
 	// clear screen
-	SDL_SetRenderDrawColor(mRenderer, 0, 0, 255, 255);
-	SDL_RenderClear(mRenderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
 
 	// draw walls
-	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-	const int thickness{20};
+	
 
 	SDL_Rect rightWall{	1024 - thickness,	 0,
 		thickness, 768 	};
-	SDL_RenderFillRect(mRenderer, &rightWall);
+	SDL_RenderFillRect(renderer, &rightWall);
 
 	SDL_Rect roof{ 0, 0, 1024, thickness	};
-	SDL_RenderFillRect(mRenderer, &roof);
+	SDL_RenderFillRect(renderer, &roof);
 
 	SDL_Rect floor{ 0, 768 - thickness,
 		1024, thickness	};
-	SDL_RenderFillRect(mRenderer, &floor);
+	SDL_RenderFillRect(renderer, &floor);
 
-	SDL_Rect ball{ static_cast<int>(ballPos.x - thickness / 2.f), static_cast<int>(ballPos.y - thickness / 2.f),
+	SDL_Rect ball{ static_cast<int>(ball_pos.x - thickness / 2.f), static_cast<int>(ball_pos.y - thickness / 2.f),
 		thickness, thickness	};
-	SDL_RenderFillRect(mRenderer, &ball);
+	SDL_RenderFillRect(renderer, &ball);
 	
-	SDL_Rect paddle{ static_cast<int>(paddlePos.x - thickness / 2.f), static_cast<int>(paddlePos.y - thickness / 2.f),
-		thickness, static_cast<int>(thickness * 5.f) };
-	SDL_RenderFillRect(mRenderer, &paddle);
+	SDL_Rect paddle{ static_cast<int>(paddle_pos.x - thickness / 2.f), static_cast<int>(paddle_pos.y - paddle_h / 2.f),
+		thickness, paddle_h };
+	SDL_RenderFillRect(renderer, &paddle);
 	
-	SDL_RenderPresent(mRenderer);
+	SDL_RenderPresent(renderer);
 }
 
 bool initialize()
@@ -73,18 +120,18 @@ bool initialize()
 		return false;
 	}
 
-	mWindow = SDL_CreateWindow("Pong", 100, 100, 1024, 768, 0);
+	window = SDL_CreateWindow("Pong", 100, 100, 1024, 768, 0);
 
-	if (!mWindow)
+	if (!window)
 	{
 		SDL_Log("Unable to create window SDL: %s", SDL_GetError());
 
 		return false;
 	}
 
-	mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	if (!mRenderer)
+	if (!renderer)
 	{
 		SDL_Log("Unable to create renderer SDL: %s", SDL_GetError());
 
@@ -96,7 +143,7 @@ bool initialize()
 
 void run_loop()
 {
-	while (mIsRunning)
+	while (is_running)
 	{
 		process_input();
 
@@ -108,9 +155,9 @@ void run_loop()
 
 void shutdown()
 {
-	SDL_DestroyRenderer(mRenderer);
+	SDL_DestroyRenderer(renderer);
 	
-	SDL_DestroyWindow(mWindow);
+	SDL_DestroyWindow(window);
 	
 	SDL_Quit();
 }
